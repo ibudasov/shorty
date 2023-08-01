b99bc67b2c730bbec895cdd3963a83087b9006dc
Igor Budasov <igor.budasov@gmail.com>
2023-08-01 20:29:09 +0200

    gateway draft

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
-# shorty
+# Shorty
 A URL shortening service. Made in a way you don't expect it
 
 # Security improvements
 - cut all the excessive permissions from the terraform account on AWS
+- authentication/authorization for the app
 
 # Improvements backlog (non prioritized)
 - split terraform modules
 - introduce environments, now it is just `prod`
 - proper versioning for CD, now it is just hardcoded
+- introduce variables in `*.tf` where it makes sense, now it is just hardcoded, because it is the end of the day
---
+++ provisionining/gateway.tf
@@ -0,0 +1,65 @@
+resource "aws_api_gateway_rest_api" "shorty" {
+  name        = "ServerlessShorty"
+  description = "Serverless Application Shorty"
+}
+
+resource "aws_api_gateway_resource" "proxy" {
+  rest_api_id = "${aws_api_gateway_rest_api.shorty.id}"
+  parent_id   = "${aws_api_gateway_rest_api.shorty.root_resource_id}"
+  path_part   = "{proxy+}" // this resource will match any request path
+}
+
+resource "aws_api_gateway_method" "proxy" {
+  rest_api_id   = "${aws_api_gateway_rest_api.shorty.id}"
+  resource_id   = "${aws_api_gateway_resource.proxy.id}"
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda" {
+  rest_api_id = "${aws_api_gateway_rest_api.shorty.id}"
+  resource_id = "${aws_api_gateway_method.proxy.resource_id}"
+  http_method = "${aws_api_gateway_method.proxy.http_method}"
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "${aws_lambda_function.shorty.invoke_arn}"
+}
+
+resource "aws_api_gateway_method" "proxy_root" {
+  rest_api_id   = "${aws_api_gateway_rest_api.shorty.id}"
+  resource_id   = "${aws_api_gateway_rest_api.shorty.root_resource_id}"
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda_root" {
+  rest_api_id = "${aws_api_gateway_rest_api.shorty.id}"
+  resource_id = "${aws_api_gateway_method.proxy_root.resource_id}"
+  http_method = "${aws_api_gateway_method.proxy_root.http_method}"
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "${aws_lambda_function.shorty.invoke_arn}"
+}
+
+resource "aws_api_gateway_deployment" "shorty" {
+  depends_on = [
+    aws_api_gateway_integration.lambda,
+    aws_api_gateway_integration.lambda_root,
+  ]
+
+  rest_api_id = "${aws_api_gateway_rest_api.shorty.id}"
+  stage_name  = "test"
+}
+
+resource "aws_lambda_permission" "apigw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.shorty.function_name}"
+  principal     = "apigateway.amazonaws.com"
+
+  # The /*/* portion grants access from any method on any resource
+  # within the API Gateway "REST API".
+  source_arn = "${aws_api_gateway_rest_api.shorty.execution_arn}/*/*"
+}
