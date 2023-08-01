024e056f2aa044dd243b783f10149dab00122668
Igor Budasov <igor.budasov@gmail.com>
2023-08-01 19:56:45 +0200

    one more bucket for the app

--- provisionining/main.tf
+++ provisionining/main.tf
@@ -12,10 +12,24 @@ provider "aws" {
   shared_credentials_files = ["/Users/igor/.aws/credentials"]
 }
 
-module "s3_bucket" {
+module "s3_bucket_storage" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
-  bucket = "shorty-prod"
+  bucket = "shorty-prod-storage"
+  acl    = "public-read"
+
+  control_object_ownership = true
+  object_ownership         = "ObjectWriter"
+
+  versioning = {
+    enabled = true
+  }
+}
+
+module "s3_bucket_app" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "shorty-prod-app"
   acl    = "private"
 
   control_object_ownership = true
