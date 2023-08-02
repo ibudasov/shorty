e91a76e11dcf166df62009840964399a18808c23
Igor Budasov <igor.budasov@gmail.com>
2023-08-02 19:01:04 +0200

    lambda uses ECR as a source

--- provisionining/lambda.tf
+++ provisionining/lambda.tf
@@ -1,5 +1,14 @@
+resource "aws_ecr_repository" "shorty_ecr_repo" {
+  name                 = "igor-shorty"
+  // todo: make it immutable, when proper versioning and CI/CD is there
+  image_tag_mutability = "MUTABLE"
+}
+
 resource "aws_lambda_function" "shorty" {
   function_name = "ServerlessShorty"
+  timeout       = 5 # seconds
+  package_type  = "Image"
+  image_uri     = "${aws_ecr_repository.shorty_ecr_repo.repository_url}:prod"
 
   s3_bucket = "shorty-prod-app"
   s3_key    = "v1.0.0/shorty.zip"
@@ -8,24 +17,27 @@ resource "aws_lambda_function" "shorty" {
   runtime = "nodejs8.10"
 
   role = "${aws_iam_role.shorty_lambda_exec.arn}"
+
+  environment {
+    variables = {
+      ENVIRONMENT = "prod"
+    }
+  }
 }
+
 
 resource "aws_iam_role" "shorty_lambda_exec" {
   name = "serverless_shorty_lambda"
 
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
+  assume_role_policy = jsonencode({
+    Statement = [
+      {
+        Action    = "sts:AssumeRole"
+        Effect    = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
       },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
+    ]
+  })
 }
