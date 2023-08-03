848196bec65943e02dd2c3404f968afe06ab8d0f
Igor Budasov <igor.budasov@gmail.com>
2023-08-03 14:57:08 +0200

    correct address for the ECR

--- delivering/cd.sh
+++ delivering/cd.sh
@@ -6,7 +6,7 @@ docker build -t shorty . --platform=linux/amd64
 # curl "http://localhost:9000/2015-03-31/functions/function/invocations" -d '{}'
 
 # authenticate ECR
-aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin public.ecr.aws/o4b6l6k8/igor-shorty
+aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin 014273152590.dkr.ecr.eu-west-1.amazonaws.com/shorty
 # push the image to ECR
 docker tag shorty:latest 014273152590.dkr.ecr.eu-west-1.amazonaws.com/shorty:latest
 docker push 014273152590.dkr.ecr.eu-west-1.amazonaws.com/shorty:latest
