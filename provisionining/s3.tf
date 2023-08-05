c8e4cbeec87aa5e7349430945faf93834c44a1bd
Igor Budasov <igor.budasov@gmail.com>
2023-08-05 03:49:57 +0200

    public_read_access

--- provisionining/s3.tf
+++ provisionining/s3.tf
@@ -25,3 +25,44 @@ resource "aws_s3_bucket_acl" "acl" {
   bucket = aws_s3_bucket.shorty_bucket.id
   acl    = "public-read"
 }
+
+resource "aws_s3_bucket_website_configuration" "example" {
+  bucket = aws_s3_bucket.shorty_bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+#https://registry.terraform.io/providers/-/aws/latest/docs/resources/s3_bucket_acl
+
+#Resource to add bucket policy to a bucket
+resource "aws_s3_bucket_policy" "public_read_access" {
+  bucket = aws_s3_bucket.shorty_bucket.id
+  policy = data.aws_iam_policy_document.public_read_access.json
+}
+
+#DataSource to generate a policy document
+data "aws_iam_policy_document" "public_read_access" {
+  statement {
+    principals {
+      type = "*"
+      identifiers = ["*"]
+    }
+
+    actions = [
+      "s3:GetObject",
+      "s3:ReadObject",
+      "s3:ListBucket",
+    ]
+
+    resources = [
+      aws_s3_bucket.shorty_bucket.arn,
+      "${aws_s3_bucket.shorty_bucket.arn}/*",
+    ]
+  }
+}
