f8e95d0047aa26dba8132a9f3754f61f3c682981
Igor Budasov <igor.budasov@gmail.com>
2023-08-05 04:31:00 +0200

    package.json scripts for automation

--- delivering/cd.sh
+++
@@ -1,8 +0,0 @@
-# build image
-docker build -t shorty . --platform=linux/amd64
-
-# authenticate ECR
-aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin 014273152590.dkr.ecr.eu-west-1.amazonaws.com/shorty
-# push the image to ECR
-docker tag shorty:latest 014273152590.dkr.ecr.eu-west-1.amazonaws.com/shorty:latest
-docker push 014273152590.dkr.ecr.eu-west-1.amazonaws.com/shorty:latest
--- delivering/test-locally.sh
+++
@@ -1,6 +0,0 @@
-# load .env file
-export $( grep -vE "^(#.*|\s*)$" .env )
-# build image just before testing
-docker build -t shorty .
-echo '✅ curl "http://localhost:9000/2015-03-31/functions/function/invocations" -d "{}"'
-docker run -p 9000:8080 -e "AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}" -e "AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}" shorty
